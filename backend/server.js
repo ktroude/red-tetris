f04b43f1e189ef1ef467bbc820da4ec5563ef001
Kevin Troude <ktroude@c4r1p4.42nice.fr>
2024-09-16 14:20:15 +0200

    add dockerfiles

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ soloGameSocket(io);
 multiGameSocket(io);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const ADRESSE = process.env.ADRESSE || "localhost";
+
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App-Input/AppInput.js
+++ frontend/src/App-Input/AppInput.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './AppInput.css'
 
 function AppInput({ placeholder, label, onChange }) {
